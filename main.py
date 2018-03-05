f95208026563a149d28e1b4d03ba6c5b906bf42b
OIdiotLin <lhd1997@qq.com>
2018-03-05 19:37:44 +0800

    add logger & save/load parameters

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import logging
 import sys
 import matplotlib as mpl
 import numpy as np
 import time
+import model_op as op
 
 from dataset import get_categories
 from parser import Parser
+from model import model_list
 
+
+# Logging Settings
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)-8s: %(message)s'))
+logger.addHandler(handler)
 
 parser = Parser()
 categories = get_categories('./data')
@@ -14,8 +24,20 @@ args, _ = parser.parse_known_args(sys.argv[1:])
 mpl.rcParams['font.sans-serif'] = ['Microsoft YaHei']
 mpl.rcParams['axes.unicode_minus'] = False
 mpl.rcParams['figure.figsize'] = (8, 6)
-np.random.seed(time.time())
+np.random.seed(int(time.time()))
 
 
 if __name__ == '__main__':
-    pass
+    net = model_list[args.model](args.category_size)
+
+    if args.model_path is not None:
+        op.load_param(net, args.model_path)
+        logger.info('load success from {0}'.format(args.model_path))
+
+    if args.op == 'info':
+        print(net)
+    elif args.op == 'train':
+        pass
+    elif args.op == 'inference':
+        pass
+
--- model_op.py
+++ model_op.py
@@ -1,3 +1,6 @@
+import torch
+
+
 def train(model, args):
     pass
 
@@ -6,5 +9,10 @@ def reference(model, args):
     pass
 
 
-def load(model, path):
-    pass
+def load_param(model, path):
+    model.load_state_dict(torch.load(path))
+
+
+def save_param(model, path):
+    torch.save(model.state_dict(), path)
+
