3e9c1c623a244271a3635582986a97482f50bc51
OIdiotLin <lhd1997@qq.com>
2018-03-05 19:03:48 +0800

    finish get_train_valid_loader()

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 import numpy as np
+from torch.utils.data import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision import transforms
 from torchvision.datasets.folder import find_classes
@@ -31,10 +32,24 @@ def get_train_valid_loader(path, scale_size, batch_size, valid_size, num_workers
         transforms.ToTensor(),
     ])
 
-    train_dataset = ImageFolder(path)
-    valid_dataset = ImageFolder(path)
+    train_dataset = ImageFolder(path, transform=train_transform)
+    valid_dataset = ImageFolder(path, transform=valid_transform)
     indices = list(range(len(train_dataset)))
     pivot = int(np.floor(valid_size * len(train_dataset)))
+
+    if shuffle:
+        np.random.shuffle(indices)
+
+    train_sampler, valid_sampler = SubsetRandomSampler(indices[:pivot]), SubsetRandomSampler(indices[pivot:])
+    train_loader = DataLoader(train_dataset,
+                              batch_size=batch_size,
+                              sampler=train_sampler,
+                              num_workers=num_workers,)
+    valid_loader = DataLoader(valid_dataset,
+                              batch_size=batch_size,
+                              sampler=valid_sampler,
+                              num_workers=num_workers,)
+    return train_loader, valid_loader
 
 
 def get_categories(path):
