8ed616d49700c82853389be6f29ecd69a5726e31
OIdiotLin <lhd1997@qq.com>
2018-03-05 19:04:42 +0800

    add argument: num-workers

--- parser.py
+++ parser.py
@@ -7,6 +7,7 @@ class Parser(argparse.ArgumentParser):
         self.add_argument('op', type=str, choices=['train', 'inference', 'info'],
                           help='the operation of model')
 
+        # Model & Dataset Settings
         self.add_argument('--model', type=str, default=None, required=True,
                           help='the name of model')
         self.add_argument('--model-path', type=str, default=None,
@@ -17,18 +18,20 @@ class Parser(argparse.ArgumentParser):
                           help='the size of images in dataset')
         self.add_argument('--category-size', type=int, default=24,
                           help='the number of categories')
+        self.add_argument('--valid', dest='valid_pct', type=float, default=0.2,
+                          help='percentage split of dataset used for validation')
+        self.add_argument('--num-workers', type=int, default=6,
+                          help='how many processes used for data loading')
 
+        # Training Settings
         self.add_argument('--lr', dest='learning_rate', type=float, default=0.0003,
                           help='learning rate when training')
         self.add_argument('--momentum', type=float, default=0.95,
                           help='momentum in SGD optimization')
-        self.add_argument('--batch-size', type=int, default=8,
-                          help='how many samples per batch to load')
         self.add_argument('--epoch', type=int, default=80,
                           help='how many epochs to train')
-        self.add_argument('--valid', dest='valid_pct', type=float, default=0.2,
-                          help='percentage split of dataset used for validation')
-
+        self.add_argument('--batch-size', type=int, default=8,
+                          help='how many samples per batch to load')
         self.add_argument('--gpu', action='store_true', default=False,
                           help='whether the model runs on GPU via CUDA')
         # self.add_argument('')
