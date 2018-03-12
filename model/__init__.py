9da27fd41957d05ba7909eddcfd7aaa6617dc786
OIdiotLin <lhd1997@qq.com>
2018-03-12 20:37:01 +0800

    add VGG-16

--- model/__init__.py
+++ model/__init__.py
@@ -1,8 +1,10 @@
 from model.alexnet import AlexNet
 from model.model1 import Model1
+from model.vgg16 import VGG16
 
 
 model_list = {
     'Model1': Model1,
     'AlexNet': AlexNet,
+    'VGG-16': VGG16,
 }
